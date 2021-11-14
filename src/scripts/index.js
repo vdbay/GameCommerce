3a5cb422fe0e88acfa6f36aac8ee594698d39b2b
vdbay <azharsubhan97@gmail.com>
2021-11-14 19:49:01 +0700

    Final Homepage MLBB

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,12 +6,14 @@ var denom = "0";
 var selector;
 var label;
 var nick = "-";
+var sisadm = "0";
 beliButton.addEventListener("click", function() {
     id = document.querySelector("#inputID").value;
     server = document.querySelector("#inputServer").value;
     nick = document.querySelector("#inputNick").value;
     harga = document.querySelector('input[type=radio][name=denom]:checked').value;
     radios = document.querySelectorAll('input[type=radio][name=denom]');
+    sisadm = document.querySelector("#inputSisaDM").value;
     denom = "0";
     for (var i = 0; i < radios.length; i++) {
         selector = 'label[for=' + radios[i].id + ']';
@@ -25,7 +27,7 @@ beliButton.addEventListener("click", function() {
     console.log(nick);
     console.log(harga);
     console.log(denom);
-    window.open("https://api.whatsapp.com/send?phone=6287784776041&text=*_FORMAT%20PEMESANAN_*%0A" + "ID%20(Server)%20%3A%20%0A" + id + "Nickname%20%3A%20%0A" + nick + "Order%20%3A%20%0A" + denom + "Sisa%20diamond%20%3A%0A" + "%0A" + "SS%20Profil%20%3A%0A" + "SS%20DM%20Sebelum%20%3A%0A" + "SS%2FFoto%20Bukti%20Bayar%20%3A%0A" + "%0A" + "*_TIDAK%20LENGKAP%3F%20TIDAK%20AKAN%20DI%20PROSES!_*")
+    window.open("https://api.whatsapp.com/send?phone=6287784776041&text=*_FORMAT%20PEMESANAN_*%0AID%20(Server)%20%3A%20" + id + " (" + server + ")%0ANickname%20%3A%20" + nick + "%0AOrder%20%3A%20" + denom + "%0ASisa%20Diamond%20%3A%20" + sisadm + "%0AHarga%20dalam%20Rupiah%20%3A%20Rp" + harga + "%0A%0A%0ASS%20Profil%20%3A%0ASS%20DM%20Sebelum%20%3A%0ASS%2FFoto%20Bukti%20Bayar%20%3A%0A%0A*_TIDAK%20LENGKAP%3F%20TIDAK%20AKAN%20DI%20PROSES!_*")
 });
 var hargatopup = document.querySelector("#harga");
 var rad = document.querySelectorAll('input[type=radio][name=denom]');
